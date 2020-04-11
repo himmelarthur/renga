fa60170db15cae0b628afd696bd9253c4f4fcf6a
Vincent Poulain <vincent.poulain2@gmail.com>
2020-04-11 20:27:10 +0200

    Explicit token sign userId / partyId

--- api/.src/types/Mutation.ts
+++ api/.src/types/Mutation.ts
@@ -7,23 +7,57 @@ export const Mutation = mutationType({
     definition(t) {
         t.crud.createOneUser()
         t.crud.createOneRenga()
+        t.field('createSubmission', {
+            type: 'Submission',
+            args: {
+                rengaId: intArg({ required: true }),
+                movieDBId: intArg({ required: true }),
+                movieTitle: stringArg({ required: true }),
+            },
+            resolve: async (
+                _,
+                { rengaId, movieDBId, movieTitle },
+                context: Context
+            ) => {
+                const res = await context.prisma.renga.findMany({
+                    where: { AND: [{ id: rengaId }, { movie: { movieDBId } }] },
+                })
+                const user = await context.user
+                if (user?.userId === undefined) {
+                    throw Error('User should be authenticated')
+                }
+                return await context.prisma.submission.create({
+                    data: {
+                        author: { connect: { id: user.userId } },
+                        renga: { connect: { id: rengaId } },
+                        movieDBId,
+                        movieTitle,
+                        valid: !!res.length,
+                    },
+                })
+            },
+        })
         t.field('createParty', {
             type: 'String',
             args: {
                 username: stringArg({ required: true }),
             },
             resolve: async (_, { username }, context: Context) => {
-                const party = await context.prisma.party.create({
+                const user = await context.prisma.user.create({
+                    select: { partyId: true, id: true },
                     data: {
-                        id: Math.random().toString(36).substr(2, 9),
-                        users: {
+                        party: {
                             create: {
-                                username,
+                                id: Math.random().toString(36).substr(2, 9),
                             },
                         },
+                        username,
                     },
                 })
-                return sign({ username, party }, appSecret())
+                return sign(
+                    { userId: user.id, partyId: user.partyId },
+                    appSecret()
+                )
             },
         })
     },
