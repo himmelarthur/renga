5be472febd6d0b47cec40f9262e943dfd89d7161
Vincent Poulain <vincent.poulain2@gmail.com>
2020-04-12 23:03:22 +0200

    Display join party or let's party!

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,20 @@ import JwtDecode from 'jwt-decode'
 import Join from './Join'
 
 export default () => {
-    const [ready, setReady] = useState(false)
     const [userId, setUserId] = useState<number>()
     useEffect(() => {
         const token = localStorage.getItem('token')
-        try {
-            if (token === null) {
-                setReady(true)
-                return
-            }
+        if (token) {
             const { userId } = JwtDecode(token)
             setUserId(userId)
-        } finally {
-            setReady(true)
         }
     }, [])
-    if (!ready) {
-        return <div></div>
-    }
     return (
         <AuthContext.Provider value={{ userId, setUserId }}>
             <Router>
                 <Switch>
                     <Route path="/p/:partyId">
-                        <Party></Party>
-                    </Route>
-                    <Route path="/join">
-                        <Join></Join>
+                        {userId ? <Party /> : <Join />}
                     </Route>
                     <Route path="/" exact>
                         <Landing></Landing>
