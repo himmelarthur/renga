1e736c59f2665d309ce9499b20df13279e895bdc
Arthur Himmel <himmel.arthur@gmail.com>
2020-04-12 16:08:51 +0200

    Posted by 'you'

--- src/components/Renga.tsx
+++ src/components/Renga.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Renga as RengaType, User } from '../generated/graphql'
 import { Emoji } from 'emoji-mart'
 import moment from 'moment'
+import { AuthContext } from '../AuthContext'
 
-const Renga = ({ renga, onClick }: Props) => (
-    <div
-        className="flex flex-col justify-center items-center w-40 mr-4 mb-4 rounded hover:bg-gray-200 p-2 cursor-pointer"
-        onClick={onClick}
-    >
-        <div className="flex flex-row justify-around">
-            {renga.emojis.map((emoji, index) => (
-                <Emoji size={32} native emoji={emoji} key={index} />
-            ))}
+const Renga = ({ renga, onClick }: Props) => {
+    const { userId } = useContext(AuthContext)
+    return (
+        <div
+            className="flex flex-col justify-center items-center w-40 mr-4 mb-4 rounded hover:bg-gray-200 p-2 cursor-pointer"
+            onClick={onClick}
+        >
+            <div className="flex flex-row justify-around">
+                {renga.emojis.map((emoji, index) => (
+                    <Emoji size={32} native emoji={emoji} key={index} />
+                ))}
+            </div>
+            <div className="flex items-center justify-center text-center text-gray-700 text-xs">
+                Posted by{' '}
+                {renga.author.id === userId ? 'You' : renga.author.username}{' '}
+                {moment(renga.createdAt).fromNow()}
+            </div>
         </div>
-        <div className="flex items-center justify-center text-center text-gray-700 text-xs">
-            Posted by {renga.author.username}{' '}
-            {moment(renga.createdAt).fromNow()}
-        </div>
-    </div>
-)
+    )
+}
 
 type Props = {
     renga: Pick<RengaType, 'id' | 'emojis' | 'createdAt'> & {
