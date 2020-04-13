4f49584df91800ac700a6f483671df70157d41fb
Vincent Poulain <vincent.poulain2@gmail.com>
2020-04-13 15:49:23 +0200

    Added Sentry

--- api/server.ts
+++ api/server.ts
@@ -2,11 +2,15 @@ import { ApolloServer } from 'apollo-server'
 import { applyMiddleware } from 'graphql-middleware'
 import { createContext } from './.src/context'
 import { schema } from './.src/schema'
+import * as Sentry from '@sentry/node'
 import dotenv from 'dotenv'
 import logger from './.src/logging'
 import { permissions } from './.src/security/permissions'
-
 dotenv.config()
+
+Sentry.init({
+    dsn: 'https://dbbff303002a4521a2de4c7bffac69bb@sentry.io/1841359',
+})
 
 const schemaMiddleware = applyMiddleware(schema, permissions)
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,13 @@ import './index.css'
 import './tailwind.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import * as Sentry from '@sentry/browser'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { client } from './client'
+
+Sentry.init({
+    dsn: 'https://dbbff303002a4521a2de4c7bffac69bb@sentry.io/1841359',
+})
 
 ReactDOM.render(
     <React.StrictMode>
