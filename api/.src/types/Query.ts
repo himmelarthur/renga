c8a2b80ba8b33a6a98900b8a70e03a4f9c168753
Vincent Poulain <vincent.poulain2@gmail.com>
2020-04-13 12:45:27 +0200

    Handle bad rebase

--- api/.src/types/Query.ts
+++ api/.src/types/Query.ts
@@ -1,7 +1,4 @@
-import { queryType, stringArg } from '@nexus/schema'
-import { Context } from '../context'
-import { sign } from 'jsonwebtoken'
-import { appSecret } from '../security/authentication'
+import { queryType } from '@nexus/schema'
 
 export const Query = queryType({
     definition(t) {
@@ -9,21 +6,5 @@ export const Query = queryType({
         t.crud.renga()
         t.crud.rengas({ filtering: true, ordering: true })
         t.crud.party()
-        t.field('invitePartyLink', {
-            type: 'String',
-            args: {
-                partyId: stringArg({ required: true }),
-            },
-            resolve: async (_, { partyId }, ctx: Context) => {
-                const user = await ctx.user
-                if (user?.partyId !== partyId) throw new Error("Authenticated user can only invite for current party")
-                    return `joinParty?token=${sign(
-                        {
-                            partyId,
-                        },
-                        appSecret()
-                    )}`
-            },
-        })
     },
 })
