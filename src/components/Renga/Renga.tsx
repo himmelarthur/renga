60f5ee3c35dc6d87190976954814ffd4d901e3e9
Arthur Himmel <himmel.arthur@gmail.com>
2020-04-18 15:18:55 +0200

    Fixes scrolling

--- src/components/Renga/Renga.tsx
+++ src/components/Renga/Renga.tsx
@@ -37,7 +37,7 @@ const Renga = ({ open, renga, onClick, onClose, onSolved }: Props) => {
                               if (ref.current) {
                                   ref.current.scrollIntoView({
                                       behavior: 'smooth',
-                                      block: 'nearest',
+                                      block: 'center',
                                   })
                               }
                           }, 0)
