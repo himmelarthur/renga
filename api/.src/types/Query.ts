4611b0d05f64f1153c37c770de3597c70304d11d
Vincent Poulain <vincent.poulain2@gmail.com>
2020-04-18 16:33:59 +0200

    Add user resolver

--- api/.src/types/Query.ts
+++ api/.src/types/Query.ts
@@ -2,6 +2,7 @@ import { queryType } from '@nexus/schema'
 
 export const Query = queryType({
     definition(t) {
+        t.crud.user()
         t.crud.renga()
         t.crud.rengas({ filtering: true, ordering: true })
         t.crud.party()
