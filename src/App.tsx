a8812438c17598d40429a279943049d254a8930f
Arthur Himmel <himmel.arthur@gmail.com>
2020-04-21 16:09:14 +0200

    Sets a static UID on localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Route, Switch, useLocation } from 'react-router-dom'
 import Landing from './Landing/Landing'
 import Party from './Party'
 import { AuthContext, User } from './AuthContext'
+import { track, useStaticUID } from './utils/tracking'
 
 export default () => {
     const [user, setUser] = useState<User>()
     const location = useLocation()
+    const staticUID = useStaticUID()
     useEffect(() => {
-        window.heap?.track('View Page', {
+        track('View Page', {
             page: location.pathname,
         })
     }, [location])
+    useEffect(() => {
+        window.heap?.addUserProperties({ staticUID })
+    }, [])
     return (
         <AuthContext.Provider value={{ user, setUser }}>
             <Switch>
--- src/utils/tracking.ts
+++ src/utils/tracking.ts
@@ -1,3 +1,24 @@
+import { v4 } from 'uuid'
+
 export const track = (event: string, args?: {}) => {
     window.heap?.track(event, args)
+}
+
+export const useStaticUID = () => {
+    // Generate and sets a party-invariant user id in the local storage if not present
+    // and returns it
+    const getStoredUID = () => {
+        try {
+            return localStorage.getItem('staticUID')
+        } catch (err) {
+            return null
+        }
+    }
+    const storedUID = getStoredUID()
+    if (storedUID === null) {
+        const uid = v4()
+        localStorage.setItem('staticUID', uid)
+        return uid
+    }
+    return storedUID
 }
