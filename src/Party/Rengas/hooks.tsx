3e54c2c2e06835b76a0a5cca4bb18476c851ca46
Arthur Himmel <himmel.arthur@gmail.com>
2020-04-26 11:59:20 +0200

    Using fetchMore en lieu of polling

--- src/Party/Rengas/hooks.tsx
+++ src/Party/Rengas/hooks.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useGetRengasQuery } from '../../generated/graphql'
 
 const POLLING_INTERVAL = Number(process.env.REACT_APP_POLL_INTERVAL) || 0
@@ -6,27 +6,43 @@ const POLLING_INTERVAL = Number(process.env.REACT_APP_POLL_INTERVAL) || 0
 export const useFetchRengas = (partyId: string, pageCount: number = 20) => {
     const [page, setPage] = useState(0)
     const [reachedEnd, setReachedEnd] = useState(false)
-    const {
-        loading,
-        data,
-        fetchMore,
-        stopPolling,
-        startPolling,
-        networkStatus,
-    } = useGetRengasQuery({
+    const { loading, data, fetchMore, networkStatus } = useGetRengasQuery({
         variables: { partyId, first: pageCount, skip: 0 },
         fetchPolicy: 'cache-and-network',
-        pollInterval: POLLING_INTERVAL,
     })
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchMore({
+                variables: {
+                    skip: 0,
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) return prev
+                    const currentRengaIds = new Set(
+                        prev.rengas.map((renga) => renga.id)
+                    )
+                    const newRengas = fetchMoreResult.rengas.filter(
+                        (renga) => !currentRengaIds.has(renga.id)
+                    )
+                    return {
+                        ...prev,
+                        rengas: [...newRengas, ...prev.rengas],
+                    }
+                },
+            })
+        }, POLLING_INTERVAL)
+        return () => clearInterval(interval)
+    }, [])
 
     return {
         loading,
         data,
         page,
         networkStatus,
+
         fetchMore: () => {
             if (reachedEnd) return
-            stopPolling()
             fetchMore({
                 variables: {
                     skip: (page + 1) * pageCount,
@@ -44,7 +60,6 @@ export const useFetchRengas = (partyId: string, pageCount: number = 20) => {
                 },
             })
             setPage(page + 1)
-            startPolling(POLLING_INTERVAL)
         },
     }
 }
