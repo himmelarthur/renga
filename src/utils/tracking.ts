72cb55b43982688060ec47db063021d43e203410
Vincent Poulain <vincent.poulain2@gmail.com>
2020-05-07 13:03:42 +0200

    Add account tracking

--- src/utils/tracking.ts
+++ src/utils/tracking.ts
@@ -1,7 +1,8 @@
 import { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { v4 } from 'uuid'
-import { PlayerContext } from '../AuthContext'
+import { PartyContext } from '../AuthContext'
+import { useAuth0 } from './auth0'
 
 export const track = (event: string, args?: {}) => {
     window.heap?.track(event, args)
@@ -29,28 +30,43 @@ export const useStaticUID = () => {
 const staticUID = useStaticUID()
 
 export const useGlobalTracking = () => {
-    const { player } = useContext(PlayerContext)
+    const { player } = useContext(PartyContext)
+    const { user } = useAuth0()
     const { partyId } = useParams()
 
     // Init trackers
     useEffect(() => {
-        if (partyId && window.$crisp && player) {
-            window.$crisp.push([
-                'set',
-                'user:nickname',
-                [`user:${player.username || player.userId.toString()}`],
-            ])
-            window.$crisp.push([
-                'set',
-                'session:data',
-                [[['partyId', partyId]]],
-            ])
-            window.heap?.identify(staticUID)
-            window.heap?.addUserProperties({
-                partyId,
-                partyUserId: player.userId,
-                ...(player.username ? { username: player.username } : {}),
-            })
+        if (window.$crisp) {
+            if (user) {
+                window.$crisp.push([
+                    'set',
+                    'session:data',
+                    [[['account', user.sub]]],
+                ])
+                window.$crisp.push(['set', 'user:email', [user.email]])
+            }
+
+            if (partyId) {
+                window.$crisp.push([
+                    'set',
+                    'session:data',
+                    [[['partyId', partyId]]],
+                ])
+            }
+
+            if (partyId && player) {
+                window.$crisp.push([
+                    'set',
+                    'user:nickname',
+                    [`user:${player.username || player.userId.toString()}`],
+                ])
+                window.heap?.identify(staticUID)
+                window.heap?.addUserProperties({
+                    partyId,
+                    partyUserId: player.userId,
+                    ...(player.username ? { username: player.username } : {}),
+                })
+            }
         }
-    }, [partyId, player])
+    }, [partyId, player, user])
 }
